34157abf7838c89b0008001eaa18fcab39ceb9b2
marfa65 <marfa.gonzalez@hotmail.com>
2021-11-25 01:33:50 -0300

    token en storage y creacion de clientes

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
 import { useRouter } from "next/router";
 
 export default function Layout({ children }) {
@@ -30,6 +31,7 @@ export default function Layout({ children }) {
           <div className="sm:flex min-h-screen">
             <Sidebar />
             <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
+              <Header />
               {children}
             </main>
           </div>
--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { gql, useMutation } from "@apollo/client";
+import { useRouter } from "next/router";
+
+const AUTENTICAR_USUARIO = gql`
+  mutation autenticarUsuario($input: AutenticarInput) {
+    autenticarUsuario(input: $input) {
+      token
+    }
+  }
+`;
 
 const Login = () => {
+  const router = useRouter();
+
+  const [mensaje, guardarMensaje] = useState(null);
+
+  // mutation para logear nuevos usuarios
+  const [autenticarUsuario] = useMutation(AUTENTICAR_USUARIO);
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("El email no es válido")
+        .required("email requerido"),
+      password: Yup.string().required("El password es obligatorio"),
+    }),
+    onSubmit: async (valores) => {
+      const { email, password } = valores;
+      try {
+        const { data } = await autenticarUsuario({
+          variables: {
+            input: {
+              email,
+              password,
+            },
+          },
+        });
+
+        guardarMensaje("Autenticando....");
+
+        // guardar el token en localstorage
+        const { token } = data.autenticarUsuario;
+        localStorage.setItem("token", token);
+        // redireccionar hacia clientes
+
+        setTimeout(() => {
+          guardarMensaje(null);
+          router.push("/");
+        }, 1500);
+      } catch (error) {
+        guardarMensaje(error.message.replace("GraphQL rror: ", ""));
+        // console.log(error);
+
+        setTimeout(() => {
+          guardarMensaje(null);
+        }, 3000);
+      }
+    },
+  });
+
+  const mostrarMensaje = () => {
+    return (
+      <div className="bg-white py-2 px-3 w-full my-3 max-w-sm text-center mx-auto">
+        <p>{mensaje}</p>
+      </div>
+    );
+  };
+
   return (
     <>
       <Layout>
         <h1 className="text-center text-2xl text-white font-normal">Login</h1>
 
+        {mensaje && mostrarMensaje()}
         <div className="flex justify-center mt-5">
           <div className="w-full max-w-sm">
-            <form className="bg-white rounded shdow-md px-8 pt-6 pb-8 mb-4">
+            <form
+              className="bg-white rounded shdow-md px-8 pt-6 pb-8 mb-4"
+              onSubmit={formik.handleSubmit}
+            >
               <div className="mb-4">
                 <label
                   className="block text-gray-700 text-sm font-bold mb-2"
@@ -22,8 +98,16 @@ const Login = () => {
                   id="email"
                   type="email"
                   placeholder="Email Usuario"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
                 />
               </div>
+              {formik.touched.email && formik.errors.email ? (
+                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                  <p>{formik.errors.email}</p>
+                </div>
+              ) : null}
               <div className="mb-4">
                 <label
                   className="block text-gray-700 text-sm font-bold mb-2"
@@ -36,8 +120,16 @@ const Login = () => {
                   id="password"
                   type="password"
                   placeholder="Password Usuario"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
                 />
               </div>
+              {formik.touched.password && formik.errors.password ? (
+                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                  <p>{formik.errors.password}</p>
+                </div>
+              ) : null}
               <input
                 type="submit"
                 className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:cursor-pointer hover:bg-gray-900"
