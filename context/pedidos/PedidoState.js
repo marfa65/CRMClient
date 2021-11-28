b14c7706ea8979f90c498af5da354524275fbde8
marfa65 <marfa.gonzalez@hotmail.com>
2021-11-27 21:52:40 -0300

    state pedidos

---
+++ context/pedidos/PedidoState.js
@@ -0,0 +1,10 @@
+import React, { useReducer } from "react";
+import PedidoContext from "./PedidoContext";
+import PedidoReducer from "./PedidoReducer";
+
+import {
+  SELECCIONAR_CLIENTE,
+  SELECCIONAR_PRODUCTO,
+  CANTIDAD_PRODUCTOS,
+  ACTUALIZAR_TOTAL,
+} from "../../types";
