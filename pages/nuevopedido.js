6cd4e9cb40793b29ba656fa2e4f871fd7b8f54b9
marfa65 <marfa.gonzalez@hotmail.com>
2021-12-02 01:10:17 -0300

    ajustes vs

--- pages/nuevopedido.js
+++ pages/nuevopedido.js
@@ -132,10 +132,31 @@ const NuevoPedido = () => {
 
           <button
             type="button"
-            className={` bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 ${validarPedido()} `}
+            className={` bg-gray-800 w-full mt-5 p-2 text-white rounded uppercase font-bold  ${validarPedido()}  transition duration-700 ease-in-out hover:bg-green-600  hover:text-black`}
             onClick={() => crearNuevoPedido()}
           >
             Registrar Pedido
+          </button>
+          <button
+            type="button"
+            className="flex justify-center items-center mt-5 w-full bg-red-700 py-2 px-4 w-auto text-white rounded text-xs uppercase font-bold transition duration-700 ease-in-out hover:bg-red-500 hover:text-black"
+            onClick={() => router.push("/pedidos")}
+          >
+            Cancelar - Volver
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 ml-3"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z"
+              />
+            </svg>
           </button>
         </div>
       </div>
